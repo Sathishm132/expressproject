5d3f6ec1fcfa4713666236ad20b7137a53d043ce
Your Name <you@example.com>
2023-05-06 18:35:51 +0530

    parser

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express=require('express');
+const bodyparser=require('body-parser')
 const app=express();
-app.use((req,res,next)=>{
-    console.log("rsponse")
-    res.send('<h1>welcome</h1>')
-})
-app.use((req,res,next)=>{
+app.use(bodyparser.urlencoded({extended:false}))
+app.use("/add-product",(req,res,next)=>{
+   
+    res.send('<form action="/product"><input type="text" name="title"></input><button>send</button><input type="text" name="size"></input><button>send</buton></form>')
     
 })
+app.use("/product",(req,res,next)=>{
+    console.log(req.body);
+    res.redirect("/")
+    
+    
+})
+
+app.use("/",(req,res,next)=>{
+    
+    res.send('<h1>welcome</h1>')
+})
+
 app.listen(4000)
