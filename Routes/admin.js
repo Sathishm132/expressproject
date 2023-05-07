6855d3443e478f26e2b874276d56972e074e5625
Your Name <you@example.com>
2023-05-07 16:33:00 +0530

    group chat

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,10 +1,14 @@
 const express=require("express");
 const Router=express.Router();
+Router.use("/login",(req,res,next)=>{
+    res.send('<form onsubmit="localStorage.setItem(`username`, document.getElementById(`username`).value)" action="/admin/product" method="POST"><input id="username" type="text" name"title"><button type="submit">add</button></form>')
+})
 Router.use("/add-product",(req,res,next)=>{
    
     res.send('<form action="/admin/product" method="POST"><input type="text" name="title"></input><input type="text" name="size"></input><button>send</buton></form>')
     
 })
+
 Router.use("/product",(req,res,next)=>{
     console.log(req.body);
     res.redirect("/")
--- Routes/shop.js
+++ Routes/shop.js
@@ -1,8 +1,20 @@
 const express=require('express');
 const router=express.Router();
+const fs = require('fs');
 router.get("/",(req,res,next)=>{
+    fs.readFile("./message.txt",(err,data)=>{
+        res.send(`${data}<form action="/" method="POST" onsubmit="document.getElementById('username').value=localStorage.getItem('username')" >
+    <input type="text" id="message" name="message"></input>
+    <input type="hidden" id="username" name="username"></input><button type="submit">send</button></form>`)
+    })
     
-    res.send('<h1>welcome</h1>')
+})
+router.use("/",(req,res,next)=>{
+    console.log(req.body.username);
+    fs.writeFile("message.txt",`${req.body.username}:${req.body.message}`,{flag:'a'},(err)=>{
+     console.log("  qho")
+    })
+   return res.redirect("/")
 })
 
 
